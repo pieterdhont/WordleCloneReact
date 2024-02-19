72c54f03c646324bdb3edc25972ea62523f25d62
Pjotterismus <dhontpieter22@gmail.com>
2024-02-19 21:41:33 +0100

    first commit

---
+++ src/Words.js
@@ -0,0 +1,24 @@
+//Words.js
+
+import wordBank from "./wordle-bank.txt";
+
+export const boardDefault = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""], 
+];
+
+export const generateWordset = async () => {
+let wordSet;
+await fetch(wordBank)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      wordSet = new Set(wordArr);
+    });
+
+    return { wordSet };
+};
